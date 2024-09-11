fbf9172b183a84d4eb37cfa094c1ced773646f71
Vương Phương Thảo <125387948+pthao12@users.noreply.github.com>
2024-09-11 18:26:23 +0700

    add Delete api

--- myproject/flashcard/urls.py
+++ myproject/flashcard/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('all/', views.getLists, name='all_list'),
     path('<str:pk>/', views.getListDetail, name='list_detail'),
-    path('word/<str:pk>/update/', views.updateWord, name='update_word')
+    path('word/<str:pk>/update/', views.updateWord, name='update_word'),
+    path('word/<str:pk>/delete/', views.deleteWord, name='delete_word'),
     # path('kanji-art/', getKanjiArt, name='kanji_art')
 ]
