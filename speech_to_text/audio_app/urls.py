e761653cace00da2b250dc360563cb655c941dcf
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-09-15 16:45:18 +0700

    chore: Merge chatbot into current project

--- speech_to_text/audio_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('record/', views.record_audio, name='record_audio'),
-    path('upload/', views.upload_audio, name='upload_audio'),
-]
