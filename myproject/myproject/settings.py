0b8c5e0be69859825fe5ddfb547624aec075e59c
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-09-18 14:40:31 +0700

    chore: Settings for deloyment

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c1)p@ck&=(r$oo755cm7rtkw=lxm1v0z*)xh7+7+g0k018hyhv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
